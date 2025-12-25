12325f4d9bebacbc0e345de0bfa9dd9ee49877e6
Weblend Developer <developer@weblend.com>
2025-12-25 22:59:08 +0100

    Fix: Enforce consistent Turntable rotation (Global Y Up) to avoid Z inversion feeling

--- js/controls/QuaternionOrbitControls.js
+++ js/controls/QuaternionOrbitControls.js
@@ -211,8 +211,8 @@ export class QuaternionOrbitControls {
 
 		const eyeDirection = this.eye.clone().normalize();
 		const upDirection = this.camera.up.clone().normalize();
-    const worldYAxis = new Vector3(0, 1, 0);
-		const worldUp = worldYAxis.clone().multiplyScalar(upDirection.dot(worldYAxis)).normalize();
+        const worldYAxis = new Vector3(0, 1, 0);
+		const worldUp = worldYAxis.clone(); // Always use global up for Turntable behavior
 
 		const rightDirection = new Vector3().crossVectors(upDirection, eyeDirection).normalize();
 
